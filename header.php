6e1c5074462bde388278331673056fe789b77b3b
Leo Gono <hello@wpmeds.com>
2013-10-10 05:17:55 +0800

    used .ir on website logo

--- header.php
+++ header.php
@@ -38,7 +38,7 @@ WebFontConfig = {
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="ir"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
