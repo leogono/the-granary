9d501c9baa694fa6cdec46a2f1ee1ee501891754
Leo Gono <hello@wpmeds.com>
2013-10-10 23:58:55 +0800

    fixed page template; added conditionals to automatically detect if page has thumbnail.

--- content-page.php
+++ content-page.php
@@ -6,12 +6,21 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	
+	
+	<div class="entry-content <?php echo (has_post_thumbnail() ? 'with-thumb' : ''); ?>">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
+	<?php if( has_post_thumbnail() ) {
+		?>
+		<div class="thumbnail-wrap">
+			<?php echo the_post_thumbnail('page-thumb'); ?>
+		</div>
+		<?php
+	} ?>
 </article><!-- #post-## -->
--- page.php
+++ page.php
@@ -13,18 +13,15 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div id="main" class="site-main clearfix" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
-				<?php if ( has_post_thumbnail() ) {
-					the_post_thumbnail();
-				} ?>
 
 			<?php endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
+		</div><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
