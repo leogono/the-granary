dccba9a56fe46106e87116d95d7d85e0d1b61e66
Leo Gono <hello@wpmeds.com>
2013-10-22 02:34:29 +0800

    updated footer markup

--- footer.php
+++ footer.php
@@ -15,13 +15,13 @@
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php if ( ! dynamic_sidebar( 'sidebar-4' ) ) ; ?>
 		</div><!-- #footer-widget -->
-		<p id="footer-text">
+		<div id="footer-text">
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 			Vestibulum consequat, orci ac laoreet cursus, dolor sem luctus lorem,
 			eget consequat magna felis a magna. Aliquam scelerisque condimentum ante, 
 			eget facilisis tortor lobortis in. In interdum venenatis justo eget consequat.
 			Morbi commodo rhoncus mi nec pharetra. Aliquam erat
-		</p>
+		</div>
 		<ul id="social-media">
 			<li class="facebook"><a href="#">facebook</a></li>
 			<li class="tumbler"><a href="#">tumbler</a></li>
